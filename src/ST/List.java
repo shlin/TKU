7d52aede12f0ba788f2d130ff7b50ea88cccc93d
PStar <pstar.lin@mail.im.tku.edu.tw>
2013-10-31 02:18:32 +0800

    資料結構 D1.
    軟體技術期中考題庫
    資料結構 d1.

--- src/ST/List.java
+++ src/ST/List.java
@@ -1,76 +1,113 @@
 package ST;
 
-
 /**
  * @author PStar
- *
+ * 
+ *         Double Linked List
+ * 
  */
 public class List {
 	private int size = 0;
-	
+
 	public Node first = null;
 	public Node last = null;
-	
-	public List(Object data){
+
+	public List(Object data) {
 		this.first = new Node(data);
 		size++;
 	}
-	
-	public List(){
+
+	public List() {
 		size = 0;
 	}
-	
-	public void addFirst(Object data){
+
+	public void addFirst(Object data) {
 		Node newNode = new Node(data);
-		
-		if(first == null){
+
+		if (first == null) {
 			first = newNode;
-		}else{
+		} else {
 			newNode.next = first;
 			first.prev = newNode;
 			first = newNode;
 		}
-		
+
 		size++;
 	}
-	
-	public void addLast(Object data){
+
+	public void addLast(Object data) {
 		Node newNode = new Node(data);
-		
-		if(first == null)
-			addFirst(data);
-		else if(last == null){
+
+		if (last == null) {
 			last = newNode;
-			last.prev = first;
-		}else{
+			if (first == null)
+				first = last;
+			else
+				last.prev = first;
+		} else {
 			last.next = newNode;
 			newNode.prev = last;
 			last = newNode;
 		}
-		
+
 		size++;
 	}
-	
-	public void showList(){
-		Node itor = first;
-		
-		while(itor != null){
-			System.out.println(itor.getData());
-			itor = itor.next;
-		}
-		
-		System.out.println("Size = " + getSize());
+
+	public void add(Object data) {
+		addLast(data);
 	}
-	
-	public int getSize(){
+
+	public Object removeFirst() {
+		Object firstData = first.getData();
+
+		first = first.next;
+		if (first.prev == null)
+			first.prev = null;
+
+		size--;
+
+		return firstData;
+	}
+
+	public Object removeLast() {
+		Object lastData = last.getData();
+
+		last = last.prev;
+		if (last.next == null)
+			last.next = null;
+
+		size--;
+
+		return lastData;
+	}
+
+	public void showList() {
+		Node itor = first;
+		int index = 0;
+
+		System.out.println("<< List Nodes >>");
+		while (itor != null && index < size) {
+			System.out.println(index + " : " + itor.getData());
+			itor = itor.next;
+
+			index++;
+		}
+
+		if (size == 0)
+			System.out.println("List is Empty!!\n");
+		else
+			System.out.println("Size = " + getSize() + "\n");
+	}
+
+	public int getSize() {
 		return this.size;
 	}
-	
-	public Node getFirst(){
+
+	public Node getFirst() {
 		return this.first;
 	}
-	
-	public Node getLast(){
+
+	public Node getLast() {
 		return this.last;
 	}
 }
--- src/ST/STMain.java
+++ src/ST/STMain.java
@@ -4,11 +4,46 @@ public class STMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+	}
+
+	public void dataStructuresD1() {
+		// New A Empty List
 		List newList = new List();
-		
-		newList.addLast("aaaa");
+
+		// Add
+		System.out.println("###### 新增物件 ######");
+		System.out.println("###### 原始List ######");
+		newList.showList();
+
 		newList.addLast(true);
+		newList.addLast('A');
+		newList.addLast(1000);
 		newList.addLast("TEST");
+
+		System.out.println("###### 結果List ######");
+		newList.showList();
+
+		// Remove First
+		System.out.println("###### 由前刪除兩物件 ######");
+		System.out.println("###### 原始List ######");
+		newList.showList();
+
+		System.out.println(">> 刪除: " + newList.removeFirst());
+		System.out.println(">> 刪除: " + newList.removeFirst());
+
+		System.out.println("###### 結果List ######");
+		newList.showList();
+
+		// Remove Last
+		System.out.println("###### 由後刪除兩物件 ######");
+		System.out.println("###### 原始List ######");
+		newList.showList();
+
+		System.out.println(">> 刪除: " + newList.removeLast());
+		System.out.println(">> 刪除: " + newList.removeLast());
+
+		System.out.println("###### 結果List ######");
 		newList.showList();
 	}
 
