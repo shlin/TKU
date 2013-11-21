0873cdb59fa9978da37883ef12386cf2aafc4ec2
PStar <pstar.lin@mail.im.tku.edu.tw>
2013-11-21 23:39:57 +0800

    資訊概論
    資訊概論 期中考 程式答案

---
+++ src/ItorToComputers/Midtern/M602630096_1.java
@@ -0,0 +1,19 @@
+package ItorToComputers.Midtern;
+
+public class M602630096_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("(a)");
+		// (a) 限用三行 println
+		System.out.println("  O/\"");
+		System.out.println(" <D");
+		System.out.println(" / \\");
+		
+		System.out.println("(b)");
+		// (b) 限用一行 print
+		System.out.print("  O/\"\n <D\n / \\\n");
+	}
+
+}
