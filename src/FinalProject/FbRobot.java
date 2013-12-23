369dde8e8801cc67c063182eea1a1017b269723a
PStar <pstar.lin@mail.im.tku.edu.tw>
2013-12-23 22:41:10 +0800

    期末專案：Facebook按讚精靈
    專案初始化

---
+++ src/FinalProject/FbRobot.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package FinalProject;
+
+/**
+ * @author PStar
+ *
+ */
+public class FbRobot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
