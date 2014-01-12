95993851a89b969d09527bc817905518f9c9f444
PStar <pstar.lin@mail.im.tku.edu.tw>
2014-01-12 22:30:18 +0800

    [期末專案] 更新 Jxbrowser License

--- src/FinalProject/YoutubePlayer.java
+++ src/FinalProject/YoutubePlayer.java
@@ -1,8 +1,6 @@
 package FinalProject;
 
 import com.teamdev.jxbrowser.chromium.*;
-import com.teamdev.jxbrowser.chromium.events.FinishLoadingEvent;
-import com.teamdev.jxbrowser.chromium.events.LoadAdapter;
 
 import javax.swing.*;
 
@@ -19,22 +17,13 @@ public class YoutubePlayer {
 		JFrame frame = new JFrame("Youtube 點播機");
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.add(browser.getView().getComponent(), BorderLayout.CENTER);
-		frame.setSize(1024, 768);
+		frame.setSize(800, 600);
 		frame.setVisible(true);
 
-		browser.addLoadListener(new LoadAdapter() {
-			@Override
-			public void onFinishLoadingFrame(FinishLoadingEvent event) {
-				if (event.isMainFrame()) {
-					// web page is loaded completely including all frames
-					String tmp = browser.getHTML();
-					System.out.println(tmp);
-				}
-			}
-		});
-
 		browser.loadURL("http://www.youtube.com/embed/fNJSUT97NZk?list=PLB27E983B2D5BC4C4");
-
+		while (browser.isLoading()) {
+		    TimeUnit.MILLISECONDS.wait(50);
+		}
 		// web page is loaded completely
 	}
 
