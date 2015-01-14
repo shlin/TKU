d41431df69aae01ec3013cc50e561f018eabb263
PStar <pstar.lin@mail.im.tku.edu.tw>
2015-01-15 05:15:11 +0800

    開始進行C17運算測試

---
+++ src/LogicGate/LGMain.java
@@ -0,0 +1,45 @@
+package LogicGate;
+
+import java.io.*;
+import java.util.*;
+
+public class LGMain {
+	private static TreeMap<Integer, Gate> gates = new TreeMap<Integer, Gate>();
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		String srcLine = "";
+		Scanner input = new Scanner(new File("exInputFile/LogicGate/Input/c17-100k.ip"));
+		
+		Iterator<Integer> itorInt;
+		Iterator<Gate> itorGate;
+		
+		BenchAnalyzer baC17 = new BenchAnalyzer(new Scanner(new File("exInputFile/LogicGate/Bench/c17.bench.txt")), gates);
+		while(input.hasNext()){
+			srcLine = input.next();
+			itorInt = baC17.getInputGates().iterator();
+			
+			int i = 0;
+			while(itorInt.hasNext())
+				gates.get(itorInt.next()).setBoolState(srcLine.charAt(i++) == '1' ? true : false);
+			
+			itorInt = gates.keySet().iterator();
+			while(itorInt.hasNext()){
+				int tmpID = itorInt.next();
+				if(!baC17.getInputGates().contains(tmpID)){
+					boolean tmpResult = false;
+					LinkedList<Integer> tmpList = gates.get(tmpID).getMember();
+					for(int listIndex = 0; listIndex < tmpList.size() - 1; listIndex++)
+						tmpResult = LogicCompute.NAND(listIndex == 0 ? gates.get(tmpList.get(listIndex)).isBoolState() : tmpResult, gates.get(tmpList.get(listIndex + 1)).isBoolState());
+					gates.get(tmpID).setBoolState(tmpResult);
+				}
+			}
+			
+			itorGate = gates.values().iterator();
+			while(itorGate.hasNext())
+				System.out.println(itorGate.next().isBoolState());
+			
+		}
+	}
+
+}
